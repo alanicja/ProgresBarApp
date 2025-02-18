d8691ec9adc9f9cbfce1ee01dfc51f341ae22f9d
alanicja <alanicja@o2.pl>
2025-02-18 01:23:34 +0100

    Dodaj pliki projektów.

---
+++ ProgresBarApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProgressBarApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // btnStart
+            this.btnStart.Location = new System.Drawing.Point(30, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // btnStop
+            this.btnStop.Location = new System.Drawing.Point(120, 30);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+
+            // btnResume
+            this.btnResume.Location = new System.Drawing.Point(210, 30);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 2;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+
+            // btnReset
+            this.btnReset.Location = new System.Drawing.Point(300, 30);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+
+            // progressBar1
+            this.progressBar1.Location = new System.Drawing.Point(30, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(345, 23);
+            this.progressBar1.TabIndex = 4;
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0%";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 150);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Progress Bar App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ ProgresBarApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProgressBarApp
+{
+    public partial class Form1 : Form
+    {
+        private Thread progressThread;
+        private bool isRunning;
+        private int progressValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                progressValue = 0;
+                label1.Text = $"{progressValue}%";
+                progressBar1.Value = progressValue;
+
+                progressThread = new Thread(UpdateProgress)
+                {
+                    IsBackground = true
+                };
+                progressThread.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                progressThread = new Thread(UpdateProgress)
+                {
+                    IsBackground = true
+                };
+                progressThread.Start();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            progressValue = 0;
+            label1.Text = $"{progressValue}%";
+            progressBar1.Value = progressValue;
+        }
+
+        private void UpdateProgress()
+        {
+            while (isRunning && progressValue < 100)
+            {
+                Thread.Sleep(100);
+                progressValue++;
+                Invoke(new Action(() =>
+                {
+                    progressBar1.Value = progressValue;
+                    label1.Text = $"{progressValue}%";
+                }));
+            }
+            isRunning = false;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            isRunning = false;
+            progressThread?.Join();
+            base.OnFormClosing(e);
+        }
+    }
+}
